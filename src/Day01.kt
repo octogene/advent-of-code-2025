7a4205ff5e060ccfa76c892eababc16916a71909
Octogene <ooctogene@gmail.com>
2025-12-04 06:54:45 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,40 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        var position = 50
+        input.forEach { rotation ->
+            val distance = rotation.substring(1).toInt()
+            position = if (rotation.first() == 'R') {
+                (position + distance) % 100
+            } else {
+                (position - distance) % 100
+            }
+            if (position == 0) count++
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0
+        var position = 50
+        input.forEach { rotation ->
+            val direction = if (rotation.first() == 'R') 1 else -1
+            val distance = rotation.substring(1).toInt()
+            repeat(distance) {
+                position = (position + direction) % 100
+                if (position == 0) count++
+            }
+        }
+        return count
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
-    // Read the input from the `src/Day01.txt` file.
+    // Read the input from the `src/Day01_test.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
